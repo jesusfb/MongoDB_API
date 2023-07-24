a5aeb37eaea7be9b0f0bbd00e3d9af99683ad492
jesusfb <jfigueroa.beltran@gmail.com>
2023-07-23 19:29:27 -0700

    Update mongodb.js

--- mongodb.js
+++ mongodb.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 // mongodb+srv://weather-station-API-user:wFHCt5G@weatherdatacluster.ky74ppd.mongodb.net/?retryWrites=true&w=majority
 const connectionString =
-    "mongodb+srv://weather-station-API-user:jZMow6VKDtHB9DDH@weatherdatacluster.ky74ppd.mongodb.net/test?proxyHost=mongodb.bypass.host&proxyPort=80&proxyUsername=student&proxyPassword=student";
+    "mongodb+srv://jesusfb:Dove3229-@cluster0.yx9sjqo.mongodb.net/api1";
 
 export const client = new MongoClient(connectionString);
 
